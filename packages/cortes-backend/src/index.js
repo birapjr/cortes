8236afebaeec10419a5b0d4c4e3c4d942513b72e
Ubirajara Cortes <birapjr@gmail.com>
2019-10-30 23:49:26 +0100

    fix(cortes-backend): typo on message

--- packages/cortes-backend/src/index.js
+++ packages/cortes-backend/src/index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hecortes backend int'));
+app.get('/', (req, res) => res.send('Cortes backend int'));
 
 app.listen(port, () => console.log(`Cortes backend app listening on port ${port}!`));
